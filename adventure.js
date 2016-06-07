118f4d9898530ab41cdbbb769ba8c65217da689f
Robert Reilley <rpreilley@gmail.com>
2016-06-07 08:19:31 -0500

    adventure.js rough

---
+++ adventure.js
@@ -0,0 +1,60 @@
+var answer = prompt("Hello. Please type your choice of either Star Wars, Lord of the Rings, or Game of Thrones.");
+
+if (answer === "Star Wars"); {
+console.log ("Good choice. Would you rather be Luke Skywalker, Darth Vader, or Yoda?"); {
+if (answer === "Luke Skywalker"); {
+console.log ("One of the last living Jedi. You weild a lightsaber and fight for what is right.");
+}
+else if (answer === "Darth Vader"); {
+console.log("You turned to the Dark Side seeking a way to bring someone back to life. You are half machine."); {
+else if (answer === "Yoda"); {
+console.log ("You are one of the most powerful Jedi in existense. After the Clone Wars, you went into hiding, and eventually would train Luke Skywalker.");
+}
+else {
+console.log("That is not a valid choice.");
+}
+
+else if (answer === "Lord of the Rings"); {
+console.log ("Good choice. Would you rather be Stryder, Elrond, or Frodo?"); {
+if (answer === "Stryder"); {
+console.log ("You help Frodo in his adventure and never betray him. Eventually you become the king of men.");
+}
+else if (answer === "Elrond"); {
+console.log("You are the leader of the elves. You have lived for hundreds of years and have fought to try to destroy the one ring."); {
+else if (answer === "Frodo"); {
+console.log ("You volunteer to destroy the one ring that your Uncle Bilbo found. You, along with your hobbit friends, venture away from the Shire for the first time ever.");
+}
+else {
+console.log("That is not a valid choice.");
+}
+
+else if (answer === "Game of Thrones"); {
+console.log ("Good choice. Would you rather be Ned Stark, Jon Snow, or Tyrian?"); {
+if (answer === "Ned Stark");
+console.log ("You are a honorable man. You have always stood next to your friend Robert Baratheon, but things dont end well for you. You end up being beheaded. Sorry :(");
+}
+else if (answer === "Jon Snow"); {
+console.log("You are a man of the Nights Watch. You help train men to fight against the wildlings, until they betray you for letting the wildlings in Castle Black. They betray and kill you, but you are resurrected by the Red Woman."); {
+else if (answer === "Tyrian"); {
+console.log ("You are the youngest of the childred of Tywin Lannester. You are incredibly smart and cunning, and end up sailing across the sea to meet with the Mother of Dragons Choose a number 1 through 3 to pick a dragon.");
+if (answer === "1"); {
+console.log("You chose Drogon, the most powerful dragon!");
+}
+else if (answer === "2"); {
+console.log("You chose Rhaegal");
+}
+else if (answer === "3"); {
+console.log("You chose Viserion");
+}
+else {
+console.log("That is not a valid response");
+}
+}
+
+else {
+console.log("That is not a valid choice.");
+}
+
+else {
+console.log("That is not a valid response.");
+}
