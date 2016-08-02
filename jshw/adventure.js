fd120804414888dff8cd365759851c1db7d0273d
Gabriel Diaz <gdiaz1410@gmail.com>
2016-08-02 08:54:11 -0500

    Submitted JS Game

---
+++ jshw/adventure.js
@@ -0,0 +1,59 @@
+// Couldn't get all my prompts to work as I wanted but they will get there
+// How to make prompt appear even nothing is entered
+name = prompt('What is your damn name?' + '');
+
+
+//wont run this prompt('Hello ' + name '.' 'Domestic or International?');
+prompt('Domestic or International?')
+var goDomestic = 'domestic';
+var goInternational = 'international';
+// try tripType instead of dom and inter
+
+if (goDomestic === 'domestic') {
+  prompt('What is your budget?')
+} else if (goInternational === 'international') {
+  prompt('What is your budget?????')
+}
+
+
+
+
+
+var budget;
+// wont run if only an if and else section
+if (budget >= 500 && budget <= 999) {
+  prompt('Texas or California?')
+} else if (budget >= 1000) {
+  prompt('Spain or South Africa?');
+}
+
+
+
+
+
+
+var stateChoice;
+// store state values
+
+if (stateChoice === 'Texas') {
+  prompt('Austin or Houston?')
+} else {
+  prompt('San Diego or San Francisco?')
+}
+
+
+
+
+
+var countryChoice;
+// store country values
+
+if (countryChoice === 'Spain') {
+  prompt('Madrid or Barcelona?')
+} else {
+  console.log('Cape Town, South Africa it is!')
+}
+
+
+
+// add accomodation for city choices
